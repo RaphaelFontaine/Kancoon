3b6ee314638889d8f6063643123b20ebaaf383ea
rfontaine24 <raphael.fontaine62@outlook.fr>
2022-11-18 12:50:22 +0100

    debut page pergola

--- components/pergolas/photo.tsx
+++ components/pergolas/photo.tsx
@@ -6,15 +6,49 @@ export default function Photo(){
             <div className="bg-white text-black text-primary-2">
                 <img src="/assets/pergolas/pergola_bandeau.jpg" alt={"Pergolas Bioclimatique posée par KANCOON en Vendée - spécialiste de la protection solaire : pergolas, stores, terrasses aux Sables d'Olonne et en Vendée"}/>
             </div>
-            <div className="overflow-y-scroll">
+
+            <div className="flex flex-col md:flex-row md:aspect-[4/3]">
                 <iframe
                     id="inlineFrameExample"
                     title="Inline Frame Example"
                     width="50%"
-                    height="auto"
+                    height="50%"
                     src={`https://www.youtube.com/embed/jP4kk6YNMr0`}
                 >
                 </iframe>
+                <div className="h-[50%] flex flex-row bg-white w-1/2 text-title-color">
+                    <div className="flex flex-row bg-white h-grey-band pl-[40px]">
+                        <div className="bg-dark-grey w-grey-band h-grey-band">
+                        </div>
+                        <div className="bg-white pl-[20px]">
+                            <div className="bg-green w-green-band h-green-band">
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-title-size flex items-center ">
+                        Exemple d'installation d'une pergola bioclimatique
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row">
+                <div className="h-[50%] flex flex-row bg-white w-1/2 text-corps-color">
+                    <div className="flex flex-row bg-white h-grey-band pl-[40px]">
+                        <div className="bg-dark-grey w-grey-band h-grey-band">
+                        </div>
+                        <div className="bg-white pl-[20px]">
+                            <div className="bg-green w-green-band h-green-band">
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-title-size flex items-center ">
+                        Exemple d'installation d'une pergola bioclimatique
+                    </div>
+                </div>
+                <div>
+                    <img src="/assets/pergolas/pergola_bandeau.jpg" alt={"Pergolas Bioclimatique posée par KANCOON en Vendée - spécialiste de la protection solaire : pergolas, stores, terrasses aux Sables d'Olonne et en Vendée"}/>
+                </div>
+                    
             </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,27 @@ module.exports = {
       'dark-grey': '#474746',
       'green': '#ADC906',
       'light-grey': '#868688',
-      'footer-grey': '#24262d'
+      'footer-grey': '#24262d',
+      'title-color': '#54595F',
+      'corps-color': '#474746'
     },
     fontFamily: {
       police: 'Poppins'
     },
-    extend: {},
+    fontSize: {
+      'title-size': '2rem',
+      'corps-size': '1rem',
+    },
+    extend: {
+      width: {
+        'grey-band': '34px',
+        'green-band': '34px'
+      },
+      height: {
+        'grey-band': '130px',
+        'green-band': '100px'
+      }
+    },
   },
   plugins: [],
 }
