b3fba464c586c969b7b57a0f125ff44f6bb1d2e6
rfontaine24 <raphael.fontaine62@outlook.fr>
2022-12-13 22:23:40 +0100

    map page contact

--- components/contact/maps.tsx
+++ components/contact/maps.tsx
@@ -2,13 +2,14 @@ import { AspectRatio } from '@mantine/core';
 
 export function MapsMap() {
   return (
-    <div className="bp:w-1/2 w-full relative pt-[56.25%] bp:pt-[28.125%] overflow-hidden ">
-            <iframe
-                    scrolling="no"
-                    src="https://www.google.com/maps?ll=46.518944,-1.781432&z=10&t=m&hl=fr&gl=US&mapclient=embed&q=19+Rue+Cl%C3%A9ment+Ader+85340+Les+Sables-d%27Olonne"
-                    title="Google map"
-                    frameBorder="10"
-            />
-        </div>
+    <div className="relative overflow-hidden px-[5%] py-[5%] flex items-center justify-center left-0 top-0 h-[100%] w-[100%]">
+        <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d351417.93941580283!2d-1.781432!3d46.518944!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48045d46fb93c03d%3A0x258c68ff48c7bdd6!2s19%20Rue%20Cl%C3%A9ment%20Ader%2C%2085340%20Les%20Sables-d&#39;Olonne%2C%20France!5e0!3m2!1sfr!2sus!4v1670965526063!5m2!1sfr!2sus" 
+            width="1000" 
+            height="525" 
+            loading="lazy" 
+            >
+        </iframe>
+    </div>
   );
 }
