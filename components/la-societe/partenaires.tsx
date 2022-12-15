41b718b6bc14d820c1dda0fe216e21f1a322487e
rfontaine24 <raphael.fontaine62@outlook.fr>
2022-12-15 13:10:25 +0100

    cercle partenaires

--- components/la-societe/partenaires.tsx
+++ components/la-societe/partenaires.tsx
@@ -3,20 +3,28 @@ export default function Partenaires(){
         <>
             <div className="bg-black flex flex-col text-white text-center p-[2%] text-partenaire-size">
                 NOS PARTENAIRES <br/>
-                <div className="flex flex-row pt-[5%] phone:pb-[2%] items-center  justify-center">
-                    <div className="flex flex-col bp:flex-row items-center w-1/2 justify-center">
-                        <div className="bg-black rounded-full border-white border-2">
-                            <a style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}  target="_blank" href="https://www.decostory.fr/izy/">
-                                <img src={"/assets/la-societe/partenaires/izy.png"} width="60%" alt={"IZY Fentres"} loading="lazy"/>
-                            </a>
-                        </div>
-                        <div className="">
-                            <a style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} target="_blank" href="https://www.agencekoxx.fr/">
-                                <img src={"/assets/la-societe/partenaires/koxx.png"} width="60%" alt={"Koxx"} loading="lazy"/>
-                            </a>
-                        </div>
+                <div className="flex flex-wrap gap-20 px-4 py-20 items-center justify-center">
+                    <div className="bg-black rounded-full border-white border-2 overflow-hidden hover:scale-110 hover:border-green hover:shadow-xl duration-500 transition-all">
+                        <a className="flex justify-center items-center w-40 h-40" target="_blank" href="https://www.decostory.fr/izy/">
+                            <img src={"/assets/la-societe/partenaires/izy.png"} className="" alt={"IZY Fentres"} loading="lazy"/>
+                        </a>
                     </div>
-                    <div className="flex flex-col bp:flex-row items-center w-1/2 justify-center ">
+                    <div className="bg-black rounded-full border-white border-2 overflow-hidden hover:scale-110 hover:border-green hover:shadow-xl duration-500 transition-all">
+                        <a className="flex justify-center items-center w-40 h-40" target="_blank" href="https://www.decostory.fr/izy/">
+                            <img src={"/assets/la-societe/partenaires/koxx.png"} alt={"Koxx"} loading="lazy"/>
+                        </a>
+                    </div>
+                    <div className="bg-black rounded-full border-white border-2 overflow-hidden hover:scale-110 hover:border-green hover:shadow-xl duration-500 transition-all">
+                        <a className="flex justify-center items-center w-40 h-40" target="_blank" href="https://www.decostory.fr/izy/">
+                            <img src={"/assets/la-societe/partenaires/reseau-entreprendre.png"} className="" alt={"IZY Fentres"} loading="lazy"/>
+                        </a>
+                    </div>
+                    <div className="bg-black rounded-full border-white border-2 overflow-hidden hover:scale-110 hover:border-green hover:shadow-xl duration-500 transition-all">
+                        <a className="flex justify-center items-center w-40 h-40" target="_blank" href="https://www.decostory.fr/izy/">
+                            <img src={"/assets/la-societe/partenaires/marchand.png"} alt={"Koxx"} loading="lazy"/>
+                        </a>
+                    </div>
+                    {/* <div className="flex flex-col bp:flex-row items-center w-1/2 justify-center ">
                         <div className="">
                             <a style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} target="_blank" href="https://www.reseau-entreprendre.org/vendee/">
                                 <img src={"/assets/la-societe/partenaires/reseau-entreprendre.png"} width="60%" alt={"Réseau Entreprendre Vendée"} loading="lazy"/>
@@ -27,7 +35,7 @@ export default function Partenaires(){
                                 <img src={"/assets/la-societe/partenaires/marchand.png"} width="60%" alt={"Marchand Construction"} loading="lazy"/>
                             </a>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
