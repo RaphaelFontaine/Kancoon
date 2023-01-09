acdcfbab42d86e882761bd2148a93411c3c157ba
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-01-09 17:07:12 +0100

    debut carrousel realisation portable

--- components/common/realisation-carousel.tsx
+++ components/common/realisation-carousel.tsx
@@ -14,9 +14,9 @@ export function RealisationCarousel({ items }: RealisationCarouselProps) {
     const slides = items.map((item) => (
         <Carousel.Slide>
             <Image 
-            src={item.image} 
-            height='576px' 
-            width='full'
+                src={item.image} 
+                height='576px' 
+                width='full'
             />
             <h1 className="text-white py-4 font-bold text-center">
                 {item.legende}
@@ -47,12 +47,12 @@ export function RealisationCarousel({ items }: RealisationCarouselProps) {
     return (
         <Carousel 
             data-animate-type="motion-safe:animate-fadeIn"
-            className="js-show-on-scroll"
+            className="js-show-on-scroll h-full phone:h-auto phone:w-auto hover:rotate-90 phone:hover:rotate-0 transition-all duration-1000"
             classNames={{
-            "indicators" : "mb-16",
-            "indicator" : "bg-white",
-            "control" : "hidden phone:flex bg-white",
-            "slide" : "relative flex flex-col",
+                "indicators" : "mb-16",
+                "indicator" : "bg-white",
+                "control" : "hidden phone:flex bg-white",
+                "slide" : "relative flex flex-col",
             }}
             slideSize= '100%'
             mx="auto" 
