2f25fa7b41b516fafaa44adf1ede5acad14a3edb
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-01-17 20:26:08 +0100

    animations sur tel : OK

--- components/common/product.tsx
+++ components/common/product.tsx
@@ -11,6 +11,22 @@ export function Showcase(props : ShowcaseProps){
     let {left, title, core, images} = props
 
     useEffect(() => {
+        const targets = document.querySelectorAll(".fadeInLeft");
+
+        const callback = function(entries : any) {
+            entries.forEach(function(entry : any) {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("motion-safe:animate-fadeInLeft");
+                  }
+            });
+        };
+
+        const observer = new IntersectionObserver(callback);
+        targets.forEach(function(target) {
+            observer.observe(target);
+        });
+    }, []);
+    useEffect(() => {
         const targets = document.querySelectorAll(".fadeInRight");
 
         const callback = function(entries : any) {
@@ -47,7 +63,7 @@ export function Showcase(props : ShowcaseProps){
 
     return(
         <div className={`flex flex-col bp:flex-row z-1 h-auto ${left ? "" : "bp:flex-row-reverse"}`}>
-            <div className={`${left ? "bg-white text-corps-color fadeInLeft" : "bg-general-grey text-white fadeInRight"} h-auto flex flex-row bp:w-1/2 w-full`}>
+            <div className={`${left ? "bg-white text-corps-color fadeInLeft" : "bg-general-grey text-white fadeInLeft bp:fadeInRight"} h-auto flex flex-row bp:w-1/2 w-full`}>
                 <div className={`${left ? "pl-band-left" : "right-0 pr-band-left"} absolute flex flex-row z-1 h-grey-band`}>
                     <div  className={`${left ? "bg-general-grey h-grey-band" : "bg-green h-green-band"}  w-grey-band`}>
                     </div>
@@ -61,12 +77,12 @@ export function Showcase(props : ShowcaseProps){
                         {title}
                     </div>
                     <div className="text-corps-size py-[12%] bp:py-[9%] lg:py-[12%] leading-phone-text bp:leading-text lg:leading-max-text">
-                        {core} 
+                        {core}
                     </div>
                 </div>
             </div>
 
-            <div data-animate-type={`${left ? "fadeInRight" : "fadeInLeft"}`} className=" h-auto bp:w-1/2 w-full">
+            <div className={`${left ? "fadeInRight" : "fadeInRight bp:fadeInLeft"} h-auto bp:w-1/2 w-full`}>
                 {
                     images.length === 1 ?
                     <img 
