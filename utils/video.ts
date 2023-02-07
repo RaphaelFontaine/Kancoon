c06f23555a85accb099e864d4726fe788c77c0c0
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-02-07 19:55:10 +0100

    videos qui demarrent automatiquement

--- utils/video.ts
+++ utils/video.ts
@@ -1,11 +1,11 @@
 export const VIDEO_LA_SOCIETE = {
-    source: "https://www.youtube.com/embed/kuhz8VYtwF8"
+    source: "https://www.youtube.com/embed/kuhz8VYtwF8?autoplay=1"
 }
 
 export const VIDEO_PERGOLA = {
-    source: "https://www.youtube.com/embed/_6l9bFXVIBg"
+    source: "https://www.youtube.com/embed/_6l9bFXVIBg?autoplay=1"
 }
 
 export const VIDEO_PARASOL = {
-    source: "https://www.youtube.com/embed/a_hSuy8W1XY"
+    source: "https://www.youtube.com/embed/a_hSuy8W1XY?autoplay=1"
 }
