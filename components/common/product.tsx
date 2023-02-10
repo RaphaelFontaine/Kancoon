e993752118f9e9d6f67ee5e3dbae9a05788ede58
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-02-10 17:43:11 +0100

    texte produit centrer

--- components/common/product.tsx
+++ components/common/product.tsx
@@ -57,42 +57,40 @@ export function Showcase(props : ShowcaseProps){
         <div className={`flex flex-col bp:flex-row z-1 h-auto ${left ? "" : "bp:flex-row-reverse"}`}>
             <div className={`${left ? "visible" : "hidden"} text-corps-color h-auto flex flex-row bp:w-1/2 w-full`}>
                 <FadeLeft>
-                        <div className="pl-band-left absolute flex flex-row z-1 h-grey-band">
-                            <div  className="bg-general-grey h-grey-band w-grey-band">
-                            </div>
-                            <div className="bg-white pl-band-between">
-                                <div className="bg-green h-green-band w-green-band">
-                                </div>
+                    <div className="pl-band-left absolute flex flex-row z-1 h-grey-band">
+                        <div  className="bg-general-grey h-grey-band w-grey-band">
+                        </div>
+                        <div className="bg-white pl-band-between">
+                            <div className="bg-green h-green-band w-green-band">
                             </div>
                         </div>
-                        <div className="flex flex-col bg-white h-full z-0 pt-[7%] px-[14%]">
-                            <div className="text-title-size text-center">
-                                {title}
-                            </div>
-                            <div className="text-corps-size py-[12%] bp:py-[8%] lg:py-[12%] leading-phone-text bp:leading-text lg:leading-max-text">
-                                {core}
-                            </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center bg-white h-full z-0 pt-[7%] px-[14%]">
+                        <div className="text-title-size text-center">
+                            {title}
                         </div>
+                        <div className="text-corps-size py-[12%] bp:py-[8%] lg:py-[12%] leading-phone-text bp:leading-text lg:leading-max-text">
+                            {core}
+                        </div>
+                    </div>
                 </FadeLeft>
             </div>
             <div className={`${left ? "hidden" : "visible"} text-white h-auto flex flex-row bp:w-1/2 w-full`}>
                 <FadeRight>
-                    <div className="bg-general-grey h-full w-full">
-                        <div className="right-0 pr-band-left absolute flex flex-row z-1 h-grey-band">
-                            <div  className="bg-green h-green-band w-grey-band">
-                            </div>
-                            <div className="bg-general-grey pl-band-between">
-                                <div className="bg-white h-grey-band w-green-band">
-                                </div>
+                    <div className="right-0 pr-band-left absolute flex flex-row z-1 h-grey-band">
+                        <div  className="bg-green h-green-band w-grey-band">
+                        </div>
+                        <div className="bg-general-grey pl-band-between">
+                            <div className="bg-white h-grey-band w-green-band">
                             </div>
                         </div>
-                        <div className="flex flex-col z-0 pt-[7%] px-[14%]">
-                            <div className="text-title-size text-center">
-                                {title}
-                            </div>
-                            <div className="text-corps-size py-[12%] bp:py-[8%] lg:py-[12%] leading-phone-text bp:leading-text lg:leading-max-text">
-                                {core}
-                            </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center bg-general-grey h-full z-0 pt-[7%] px-[14%]">
+                        <div className="text-title-size text-center">
+                            {title}
+                        </div>
+                        <div className="text-corps-size py-[12%] bp:py-[8%] lg:py-[12%] leading-phone-text bp:leading-text lg:leading-max-text">
+                            {core}
                         </div>
                     </div>
                 </FadeRight>
