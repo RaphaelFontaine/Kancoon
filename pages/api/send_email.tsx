71f07b5c733048b98c5d6fc8f3d32e762dc9c5ce
rfontaine24 <raphael.fontaine62@outlook.fr>
2023-02-16 13:51:28 +0100

    new file

---
+++ pages/api/send_email.tsx
@@ -0,0 +1,23 @@
+import { IMail } from 'models'
+import type { NextApiRequest, NextApiResponse } from 'next'
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const form_data : IMail = req.body
+    try {
+        await sgMail.send({
+            to : "raphael.fontaine62@outlook.fr",
+            from : "raphael.fontaine18@gmail.com",
+            subject: 'Nouveau formulaire rempli sur le site Kancoon.fr',
+            templateId : "d-e63def137c0e4ca0b8d9bdd3f773e931",
+            dynamic_template_data : form_data
+        })
+        res.status(200).end()
+    } catch (error) {
+        res.status(429).send({
+            message : "Une erreur est survenue..."
+        })
+    }
+}
