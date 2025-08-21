8b38f9da8087fed8bd047834d384c479b7c86e40
RaphaelFontaine <raphael.fontaine62@outlook.fr>
2025-08-21 19:56:22 +0200

    correct env var prod

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    CONTACT_EMAIL_FROM: process.env.CONTACT_EMAIL_FROM,
+    CONTACT_EMAIL_TO: process.env.CONTACT_EMAIL_TO,
+  },
 }
 
 module.exports = nextConfig
